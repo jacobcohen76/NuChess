a46d229bd59e88582eaef239139d9168005e682c
jacob <jacob@jacob-desktop>
2020-07-20 08:25:05 -0700

    c

---
+++ src/nuchess/view/TabbedView.java
@@ -0,0 +1,18 @@
+package nuchess.view;
+
+import javax.swing.JPanel;
+
+public class TabbedView
+{
+	private JPanel panel;
+	
+	public TabbedView()
+	{
+		panel = new JPanel();
+	}
+	
+	public JPanel getPanel()
+	{
+		return panel;
+	}
+}
